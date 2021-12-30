35e325ab12128ed69a7f3cbb0b72a558555c5d34
vavolf <vavolf111@gmail.com>
2021-12-30 15:36:27 +0300

    14lab

---
+++ ConsoleApp1/lab14/Classes.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14
+{
+    [Serializable]
+    abstract public class Organization
+    {
+        [NonSerialized] public string nameOfOrganization;
+        public Organization() { }
+        public Organization(string orgName)
+        {
+            nameOfOrganization = orgName;
+        }
+        public string NameOfOrganization { get => nameOfOrganization; set => nameOfOrganization = value; }
+        public abstract void Burning();
+        public override string ToString() => $"Тип объекта – {this.GetType()}, название организации – {this.nameOfOrganization}";
+    }
+    [Serializable]
+    public class Date
+    {
+        public int year;
+        public int month;
+        public int day;
+        public int Year { get => year; set => year = value; }
+        public int Month
+        {
+            get => month;
+            set
+            {
+                if (value < 1)
+                    month = 1;
+                else if (value > 12)
+                    month = 12;
+                else
+                    month = value;
+            }
+        }
+        public int Day
+        {
+            get => day;
+            set
+            {
+                if (value < 1)
+                    day = 1;
+                else if (value > 31)
+                    day = 31;
+                else
+                    day = value;
+            }
+        }
+        public Date() { }
+        public Date(int dateDay, int dateMonth, int dateYear)
+        {
+            day = dateDay;
+            month = dateMonth;
+            year = dateYear;
+        }
+        public override string ToString() => $"Тип объекта – {this.GetType()}, дата создания: {day}.{month}.{year}";
+    }
+    [Serializable]
+    public class Document : Organization
+    {
+        public bool stamp;
+        public Date date;
+        public bool IsStamped { get => stamp; set => stamp = value; }
+        public Document() { }
+        public Document(string organizationName, int dateDay, int dateMonth, int dateYear, bool isStamped)
+            : base(organizationName)
+        {
+            date = new Date(dateDay, dateMonth, dateYear);
+            stamp = isStamped;
+        }
+        public int Day
+        {
+            get => date.Day;
+        }
+        public int Month
+        {
+            get => date.Month;
+        }
+        public int Year
+        {
+            get => date.Year;
+        }
+
+        // виртуальные методы, подлежащие переопределению
+
+        virtual public void Store()
+        {
+            Console.WriteLine("Документ хранится в сейфе");
+        }
+
+        // реализация методов интерфейса
+
+        public void Change()
+        {
+            Console.WriteLine("Документ изменен");
+        }
+        public void Find()
+        {
+            Console.WriteLine("Документ найден");
+        }
+        public void Lose()
+        {
+            Console.WriteLine("Документ утерян");
+        }
+
+        public override void Burning()
+        {
+            Console.WriteLine("Oh no! The document is burning!");
+        }
+
+        // Переопределенные методы Object
+
+        /*public override string ToString() => $"Тип объекта – {this.GetType()}, организация – {this.NameOfOrganization}, " +
+            $"дата создания – {date.Day}.{date.Month}.{date.Year}, есть ли печать – " + (stamp ? "есть" : "нет");
+        public override int GetHashCode() => HashCode.Combine(this.NameOfOrganization, date.Day, date.Month, date.Year);
+        public override bool Equals(object obj)
+        {
+            if (obj is Document objectType)
+            {
+                if (this.date.Day == objectType.date.Day
+                        && this.date.Month == objectType.date.Month
+                            && this.date.Year == objectType.date.Year
+                                && this.NameOfOrganization == objectType.NameOfOrganization)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }*/
+    }
+}
---
+++ ConsoleApp1/lab14/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ 1
+
+            // Сериализация и десереализация в/из бинарный формат
+            Document doc1 = new Document("Магнит", 12, 12, 2020, true);
+            Document doc2 = new Document("Себастьян", 10, 11, 2019, false);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\document1.dat", FileMode.OpenOrCreate))
+            {
+                binaryFormatter.Serialize(fs, doc1);
+            }
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\document1.dat", FileMode.OpenOrCreate))
+            {
+                Document newdoc1 = (Document)binaryFormatter.Deserialize(fs);
+                Console.WriteLine($"название организации – {newdoc1.NameOfOrganization}, дата – {newdoc1.Day}.{newdoc1.Month}.{newdoc1.Year}, подписан? - {newdoc1.IsStamped}");
+            }
+
+            // Сериализация и десереализация в/из формат SOAP
+            SoapFormatter soapFormatter = new SoapFormatter();
+            using (FileStream fStream = new FileStream(@"D:\C#\\lab14\lab14\document2Soap.soap", FileMode.OpenOrCreate))
+            {
+                soapFormatter.Serialize(fStream, doc1);
+            }
+            using (FileStream fStream = new FileStream(@"D:\C#\lab14\lab14\document2Soap.soap", FileMode.OpenOrCreate))
+            {
+                Document newdoc2 = (Document)soapFormatter.Deserialize(fStream);
+                Console.WriteLine($"название организации – {newdoc2.NameOfOrganization}, дата – {newdoc2.Day}.{newdoc2.Month}.{newdoc2.Year}, подписан? - {newdoc2.IsStamped}");
+            }
+
+            // Сериализация и десереализация в/из формат JSON
+
+            string jsonString = JsonSerializer.Serialize(doc1);
+            using (StreamWriter sw = new StreamWriter(@"D:\C#\lab14\lab14\document.json", false))
+            {
+                sw.Write(jsonString);
+            }
+            Document newdocument = JsonSerializer.Deserialize<Document>(jsonString);
+            Console.WriteLine($"название организации – {newdocument.NameOfOrganization}, подписан? - {newdocument.IsStamped}");
+
+            // Сериализация и десереализация в/из формат XML
+
+            XmlSerializer xSer = new XmlSerializer(typeof(Document));
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\document.xml", FileMode.OpenOrCreate))
+            {
+                xSer.Serialize(fs, doc1);
+            }
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\document.xml", FileMode.OpenOrCreate))
+            {
+                Document newdoc = xSer.Deserialize(fs) as Document;
+                Console.WriteLine($"название организации – {newdoc.NameOfOrganization}, дата – {newdoc.Day}.{newdoc.Month}.{newdoc.Year}, подписан? - {newdoc.IsStamped}");
+            }
+
+            // ЗАДАНИЕ 2 - СЕРИАЛИЗАЦИЯ/ДЕСЕРЕАЛИЗАЦИЯ МАССИВА
+
+            Document[] documents = { doc1, doc2 };
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\documents.dat", FileMode.OpenOrCreate))
+            {
+                binaryFormatter.Serialize(fs, documents);
+            }
+            using (FileStream fs = new FileStream(@"D:\C#\lab14\lab14\documents.dat", FileMode.OpenOrCreate))
+            {
+                Document[] newdocuments = (Document[])binaryFormatter.Deserialize(fs);
+                Console.WriteLine(newdocuments.Length);
+            }
+
+            // ЗАДАНИЕ 3 - СЕЛЕКТОРЫ ДЛЯ XML ФАЙЛА
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"D:\C#\lab14\lab14\document.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            // выбор всех дочерних узлов
+            XmlNodeList childnodes = xRoot.SelectNodes("*");
+            foreach (XmlNode n in childnodes)
+                Console.WriteLine(n.InnerText);
+
+            XmlNode stampNode = xRoot.SelectSingleNode("stamp");
+            if (stampNode != null)
+                Console.WriteLine(stampNode.OuterXml);
+
+            // ЗАДАНИЕ 4 - LINQ TO XML, СОЗДАНИЕ XML-ФАЙЛОВ
+
+            XDocument xdoc = new XDocument();
+
+            XElement parrot1 = new XElement("parrot");
+            XAttribute parrot1NameAttr = new XAttribute("name", "Mike");
+            XElement parrot1ColorAttr = new XElement("color", "white");
+            parrot1.Add(parrot1NameAttr);
+            parrot1.Add(parrot1ColorAttr);
+
+            XElement parrot2 = new XElement("parrot");
+            XAttribute parrot2NameAttr = new XAttribute("name", "Bee");
+            XElement parrot2ColorAttr = new XElement("color", "red");
+            parrot2.Add(parrot2NameAttr);
+            parrot2.Add(parrot2ColorAttr);
+
+            // создание корневого элемента
+            XElement parrots = new XElement("parrots");
+            parrots.Add(parrot1);
+            parrots.Add(parrot2);
+            // добавляем корневой элемент в документ
+            xdoc.Add(parrots);
+            xdoc.Save(@"D:\C#\lab14\lab14\parrots.xml");
+
+            XDocument xdoc1 = XDocument.Load(@"D:\C#\lab14\lab14\parrots.xml");
+            IEnumerable<XElement> elements = xdoc1.Descendants("parrot");
+            foreach (XElement e in elements)
+                Console.WriteLine($"Элемент {e.Name}, цвет {e.Value}");
+
+            IEnumerable<XElement> all = xdoc1.Root.Descendants("parrot")
+                                                  .Where(p => p.Element("color").Value.StartsWith("r"))
+                                                  .Select(p => p);
+            foreach (XElement e in all)
+                Console.WriteLine($"Элемент {e.Name}, цвет {e.Value}");
+
+            Console.ReadKey();
+        }
+    }
+}
